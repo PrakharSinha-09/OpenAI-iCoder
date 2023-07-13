ea9601b8d252279d26a89573b66576e6c548314c
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-07-13 12:54:11 +0530

    Updated Url

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
 
     intro.style.display="none"
     chatContainer.style.display="block"
-    
+
     const data = new FormData(form)
 
     // user's chatstripe
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://icoder-r4ct.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
